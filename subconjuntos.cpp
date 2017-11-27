dd07305028af92a07293c697ad563ff49e95da7b
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2017-11-27 01:53:35 -0200

    nada dando certo

--- subconjuntos.cpp
+++ subconjuntos.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <vector>
 #include <fstream>
+#include <list>
+#include <algorithm>
 
 //identify whether an element of the set has been chosen or not
 #define SELECTED 1
@@ -13,11 +15,55 @@ typedef struct r_set{
     int selected;
 }r_set;
 
+void debug(std::vector<int> v){
+	std::cout << "[ " ;
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); ++it){
+        std::cout << *it << " " ;
+    }
+	std::cout << "] ";
+	std::cout << std::endl;
+}
+
+int enumerate(std::vector<int> v, int n, int k, int offset, std::list<int> &subsets, std::vector<std::vector<int> > &nodes){
+	if(k==0 || v.empty())
+		return 0;
+	
+	if(offset==n){
+		std::cout << "[ ";
+  		for(std::list<int>::iterator it=subsets.begin(); it!=subsets.end() ; ++it){
+        	std::cout << *it << " ";
+			std::vector<int>::iterator i = std::find(v.begin(), v.end(), *it);
+			size_t index = std::distance(v.begin(), i);
+			v.erase(v.begin() + index);
+        }
+		std::cout << "] " << std::endl;
+		if(!v.empty()){
+			std::cout << "next call: " << std::endl;
+			debug(v);
+			std::list<int>subsets2;
+			//std::cout << std::endl;
+			//debug(v);
+			enumerate(v, v.size(), k-1, 0, subsets2, nodes);
+		}	
+		//debug(v);
+		std::cout << std::endl;
+		return 0;
+	}
+	subsets.push_back(v.at(offset));
+	enumerate(v, n, k, offset+1, subsets, nodes);
+	subsets.remove(v.at(offset));
+	enumerate(v, n, k, offset+1, subsets, nodes);
+	return 0;
+} 
+
 int main(int argc, char *argv[]){
     std::fstream input;
     char *fileName;
     int id=1, n , k;
     std::vector<r_set> sets;
+	std::list<int> subsets;
+	std::vector<int> left;
+	std::vector<std::vector<int> > nodes;
     r_set aux;
     if(argc!=4){
         std::cout << "Uso: ./subconjuntos <n> <k> <arquivo de entrada>" << std::endl;
@@ -39,11 +85,10 @@ int main(int argc, char *argv[]){
         aux.id = id;
         aux.selected = NOTSELECTED;
         sets.push_back(aux);
+		left.push_back(aux.weight);
         id++;
     }
     //Print vector
-    for(std::vector<r_set>::iterator it = sets.begin(); it != sets.end(); ++it){
-        std::cout << (*it).id << " " << (*it).weight << std::endl;
-    }
+	enumerate(left, n, k, 0, subsets, nodes);
     return 0;
 }
