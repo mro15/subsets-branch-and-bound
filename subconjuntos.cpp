084ea0dae7d277394caac47bf6b631037322019d
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2017-11-28 15:21:22 -0200

    enumerando corretamente e retirando sujeiras

--- subconjuntos.cpp
+++ subconjuntos.cpp
@@ -5,16 +5,6 @@
 #include <list>
 #include <algorithm>
 
-//identify whether an element of the set has been chosen or not
-#define SELECTED 1
-#define NOTSELECTED 0
-//Set
-typedef struct r_set{
-    int id;
-    int weight;
-    int selected;
-}r_set;
-
 void debug(std::vector<int> v){
 	std::cout << "[ " ;
     for(std::vector<int>::iterator it = v.begin(); it != v.end(); ++it){
@@ -24,7 +14,7 @@ void debug(std::vector<int> v){
 	std::cout << std::endl;
 }
 
-int enumerate(std::vector<int> v, int n, int k, int offset, std::list<int> &subsets, std::vector<std::vector<int> > &nodes){
+int enumerate(std::vector<int> v, int n, int k, int offset, std::list<int> &subsets){
 	if(k==0 || v.empty())
 		return 0;
     if(k==1){
@@ -49,15 +39,15 @@ int enumerate(std::vector<int> v, int n, int k, int offset, std::list<int> &subs
 		//debug(v);
         //bound aqui
 		std::list<int>subsets2;
-		enumerate(v, v.size(), k-1, 0, subsets2, nodes);
+		enumerate(v, v.size(), k-1, 0, subsets2);
 		std::cout << std::endl;
 		return 0;
 	}
     //bound aqui
 	subsets.push_back(v.at(offset));
-	enumerate(v, n, k, offset+1, subsets, nodes);
+	enumerate(v, n, k, offset+1, subsets);
 	subsets.remove(v.at(offset));
-	enumerate(v, n, k, offset+1, subsets, nodes);
+	enumerate(v, n, k, offset+1, subsets);
 	return 0;
 }
 
@@ -65,11 +55,8 @@ int main(int argc, char *argv[]){
     std::fstream input;
     char *fileName;
     int id=1, n , k;
-    std::vector<r_set> sets;
 	std::list<int> subsets;
 	std::vector<int> left;
-	std::vector<std::vector<int> > nodes;
-    r_set aux;
     if(argc!=4){
         std::cout << "Uso: ./subconjuntos <n> <k> <arquivo de entrada>" << std::endl;
         return 0;
@@ -85,16 +72,13 @@ int main(int argc, char *argv[]){
     }
     //TODO: guloso para achar um resultado bom pra começar o branch and bound
     //TODO: branch and bound (2 bounds)
+    int aux;
     while(id<=n){
-		input >> aux.weight;
-        aux.id = id;
-        aux.selected = NOTSELECTED;
-        sets.push_back(aux);
-		left.push_back(aux.weight);
+		input >> aux;
+		left.push_back(aux);
         id++;
     }
     //Print vector
-	enumerate(left, n, k, 0, subsets, nodes);
-    std::cout << "----------------------------------------" << std::endl;
+	enumerate(left, n, k, 0, subsets);
     return 0;
 }
